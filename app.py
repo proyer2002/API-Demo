5759d8c5025c212b6d3db42223de8631c2e22ee0
Phoebe <proyer@revealdata.com>
2022-08-02 09:30:23 -0500

    Messing with deployment to heroku

--- app.py
+++ app.py
@@ -24,9 +24,7 @@ db.init_app(app)
 
 # Database Connection
 try:
-    # DATABASE_URL = os.environ['postgresql://revealdata:H0th15C0ld!!@mastermsa-db-001.cidxb3vnerlr.us-east-1.rds.amazonaws.com:5432/master_msa']
-    # conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-    conn = "postgresql://revealdata:H0th15C0ld!!@mastermsa-db-001.cidxb3vnerlr.us-east-1.rds.amazonaws.com:5432/master_msa?sslrootcert=rds-combined-ca-bundle.pem&sslmode=require"
+    conn = "postgresql://revealdata:H0th15C0ld!!@mastermsa-db-001.cidxb3vnerlr.us-east-1.rds.amazonaws.com:5432/master_msa"
 
 
 except:
